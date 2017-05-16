4c9bbe1a7d2838fee1ec113af26d569afe600325
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-16 04:29:48 -0400

    Add event listener

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const personForm = document.querySelector('form')
 
-personForm.onsubmit = (ev) => {
-    ev.preventDefault()
-    const form = ev.target
-    const details = document.querySelector('.details')
-
-    const personName = form.personName.value
-    const hairColor = form.hairColor.value
-    const age = form.age.value
-    const birthplace = form.birthplace.value
-
-
-    const em = document.createElement('em')
-    em.textContent = personName
-
-    const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = hairColor 
-    colorDiv.style.height = '50px'
-    colorDiv.style.width = '100px' 
-
-    details.innerHTML = `
-     <ul>
-        <li>Name: ${personName}</li>
-        <li>Hair Color: ${colorDiv.outerHTML}</li>
-        <li>Age: ${age}</li>
-        <li>Birthplace: ${birthplace}</li>
-    </ul>
-    `
+const renderColor = (hairColor) => {
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = hairColor
+  colorDiv.style.height = '50px'
+  colorDiv.style.width = '100px'
+  return colorDiv
 }
+
+const handleSubmit = (ev) => {
+  ev.preventDefault()
+  const form = ev.target
+  const details = document.querySelector('.details')
+
+  const personName = form.personName.value
+  const hairColor = form.hairColor.value
+  const age = form.age.value
+  const birthplace = form.birthplace.value
+
+  const colorDiv = renderColor(hairColor)
+
+  details.innerHTML = `
+    <ul>
+      <li>Name: ${personName}</li>
+      <li>Hair Color: ${colorDiv.outerHTML}</li>
+      <li>Age: ${age}</li>
+      <li>Birthplace: ${birthplace}</li>
+    </ul>
+  `
+}
+
+personForm.addEventListener('submit', handleSubmit)
