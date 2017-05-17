dbbd78454933e97247f307a24af796e0e26f4862
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-16 21:44:15 -0400

    Create elements into a definition list

--- index.js
+++ index.js
@@ -10,34 +10,56 @@ const handleSubmit = (ev) => {
   const age = form.age.value
   const birthplace = form.birthplace.value
 
-  const list = document.createElement('ul')
-  const title = document.createTextNode('Results')
+  const list = document.createElement('dl')
+  const title = document.createTextNode('Definition List')
   list.appendChild(title)
   details.appendChild(list)
 
   const item1 = document.createElement('li')
-  const text1 = document.createTextNode('Name: '+personName)
-  item1.appendChild(text1)
+  const dt1 = document.createElement('dt')
+  const dd1 = document.createElement('dd')
+  const text1 = document.createTextNode('First Name: ')
+  const def1 = document.createTextNode(personName)
+  dt1.appendChild(text1)
+  dd1.appendChild(def1)
+  item1.appendChild(dt1)
+  item1.appendChild(dd1)
   list.appendChild(item1)
 
   const item2 = document.createElement('li')
+  const dt2 = document.createElement('dt')
+  const dd2 = document.createElement('dd')
   const text2 = document.createTextNode('Hair Color: ')
   const colorDiv = document.createElement('div')
   colorDiv.style.backgroundColor = hairColor
   colorDiv.style.height = '50px'
   colorDiv.style.width = '100px'
-  item2.appendChild(text2)
-  item2.appendChild(colorDiv)
+  dt2.appendChild(text2)
+  dd2.appendChild(colorDiv)
+  item2.appendChild(dt2)
+  item2.appendChild(dd2)
   list.appendChild(item2)
 
   const item3 = document.createElement('li')
-  const text3 = document.createTextNode('Age: '+age)
-  item3.appendChild(text3)
+  const dt3 = document.createElement('dt')
+  const dd3 = document.createElement('dd')
+  const text3 = document.createTextNode('Age: ')
+  const def3 = document.createTextNode(age)
+  dt3.appendChild(text3)
+  dd3.appendChild(def3)
+  item3.appendChild(dt3)
+  item3.appendChild(dd3)
   list.appendChild(item3)
 
   const item4 = document.createElement('li')
-  const text4 = document.createTextNode('Birthplace: '+birthplace)
-  item4.appendChild(text4)
+  const dt4 = document.createElement('dt')
+  const dd4 = document.createElement('dd')
+  const text4 = document.createTextNode('Birthplace: ')
+  const def4 = document.createTextNode(birthplace)
+  dt4.appendChild(text4)
+  dd4.appendChild(def4)
+  item4.appendChild(dt4)
+  item4.appendChild(dd4)
   list.appendChild(item4)
 
 }
