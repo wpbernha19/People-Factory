f1dfcf5074b66715677bcd50894712c9f59f72b9
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-17 23:12:18 -0400

    Create random border color on click of promote

--- Roster/rost.js
+++ Roster/rost.js
@@ -31,9 +31,13 @@ const rost = {
         list.appendChild(promoteButton)
         promoteButton.addEventListener('click', () => {
             let color = '#'; // hexadecimal starting symbol
+            let borderColor = '#'
             let letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0']; //Set your colors here
             color += letters[Math.floor(Math.random() * letters.length)]
+            borderColor += letters[Math.floor(Math.random() * letters.length)]
             list.style.backgroundColor = color
+            list.style.border = 'thick solid '+borderColor
+            //list.style.borderColor = borderColor
         })
         return list
     },
