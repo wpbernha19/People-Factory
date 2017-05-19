89775d813e299a31e2f90fa0ddacf38ca48a6998
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-19 02:54:15 -0400

    Improve look(CSS) but promote is not fixed

--- Roster/rost.js
+++ Roster/rost.js
@@ -7,31 +7,44 @@ const megaroster = {
     this.studentList = document.querySelector('#student-list')
     this.max = 0
     this.setupEventListeners()
+    this.load()
   },
 
   setupEventListeners() {
     document
       .querySelector('#new-student')
-      .addEventListener('submit', this.addStudent.bind(this))
+      .addEventListener('submit', this.addStudentViaForm.bind(this))
   },
 
-  addStudent(ev) {
+  load() {
+    const rosterString = localStorage.getItem('roster')
+    const rosterArray = JSON.parse(rosterString)
+    if (rosterArray) {
+      rosterArray
+        .reverse()
+        .map(this.addStudent.bind(this))
+    }
+  },
+
+  addStudentViaForm(ev) {
     ev.preventDefault()
     const f = ev.target
     const student = {
       id: this.max + 1,
       name: f.studentName.value,
     }
+    this.addStudent(student)
+    f.reset()
+  },
 
+  addStudent(student) {
     this.students.unshift(student)
     
-    const listItem = this.moveUp(student)
+    const listItem = this.buildListItem(student)
     this.prependChild(this.studentList, listItem)
     
     this.max ++
-    f.reset()
-
-    localStorage.setItem('roster', JSON.stringify(this.students))
+    this.save()
   },
 
   prependChild(parent, child) {
@@ -50,6 +63,30 @@ const megaroster = {
       .addEventListener('click', this.removeStudent.bind(this))
 
     return li
+  },
+
+  save() {
+      localStorage.setItem('roster', JSON.stringify(this.students))
+  },
+
+  removeStudent(ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+
+    for (let i=0; i < this.students.length; i++) {
+      let currentId = this.students[i].id.toString()
+      if (currentId === li.dataset.id) {
+        this.students.splice(i, 1)
+        break
+      }
+    }
+
+    li.remove()
+    this.save()
+  },
+
+  save() {
+      localStorage.setItem('roster', JSON.stringify(this.students))
   },
 
   editedListItem(student) {
@@ -78,10 +115,18 @@ const megaroster = {
 
   removeStudent(ev) {
     const btn = ev.target
-    btn.closest('.student').remove()
+    const li = btn.closest('.student')
 
-    // Remove it from the this.students array
-    // this.students.splice(?, 1)
+    for (let i=0; i < this.students.length; i++) {
+      let currentId = this.students[i].id.toString()
+      if (currentId === li.dataset.id) {
+        this.students.splice(i, 1)
+        break
+      }
+    }
+
+    li.remove()
+    this.save()
   },
 
   promoteStudent(ev) {
@@ -95,8 +140,6 @@ const megaroster = {
         btn.closest('.student').style.backgroundColor = color
         btn.closest('.student').style.border = 'thick solid '+borderColor
 
-        // Remove it from the this.students array
-        // this.students.splice(?, 1)
     },
 
     itemUp(ev) {
